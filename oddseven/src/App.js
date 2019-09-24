bd31f0751d1100dbdbe4069d0c9cc37214e2161d
rrchaos <snash@tutanota.com>
2019-09-24 21:24:02 +0100

    connector between App and redux-store added

--- oddseven/src/App.js
+++ oddseven/src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+import { startGame, cancelGame } from './actions/actions';
 
-
-function App(props) {  
-  return (
-    <div className="App">
-      hello world
+class App extends Component {
+  startGame = () => this.props.dispatch(startGame());
+  cancelGame = () => this.props.dispatch(cancelGame());
+  render() {
+    console.log(this)
+    return (
+      <div className="App">
+        <h2><span>🎰</span> Even or Odds <span>🎰</span></h2>
+        {
+          this.props.state.gameStarted ? (
+            <div>
+              <h3>The game is on! <span>🃏</span></h3>
+              <br/>
+              <button onClick={this.cancelGame}>Cancel Game</button>
+            </div>
+          ) : (
+            <div>
+              <h3>A new game awaits <span>🃏</span></h3>
+              <br/>
+              <button onClick={this.startGame}>Start Game</button>
+            </div>
+          )
+        }
     </div>
-  );
+    )
+  }
 }
 
-const mapStateToProps = state => console.log('state' ,state)
+//Redux special one
+const mapStateToProps = state => {
+  console.log('state on APP' ,state)
+  return { state }
+}
+//Connection between Redux Store and App.
+//mapStateToProps is just a futher way to customize the connect with redux store from our App
 const connector = connect(mapStateToProps);
 
 export default connector(App);
