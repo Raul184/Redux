bca3798ed26b554680e994626c2764181ed5a82f
Snash <raulmv01@hotmail.es>
2019-11-11 14:43:45 +0000

    Logs & Loader dumb component added

--- logger_itissues/src/App.js
+++ logger_itissues/src/App.js
@@ -2,6 +2,7 @@ import React , { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 //Needs To Be Initialized
 import M from 'materialize-css/dist/js/materialize.min.js';
+import Logs from './components/Logs/Logs.js';
 
 function App() {
   useEffect( () => {
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Logs />
     </div>
   );
 }
