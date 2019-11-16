6da62030be0305f930072431225dd8997e9df286
Snash <raulmv01@hotmail.es>
2019-11-16 20:24:34 +0000

    UpdateLogs Reducer/Actions completed

--- logger_itissues/src/actions/logActions.js
+++ logger_itissues/src/actions/logActions.js
@@ -1,4 +1,12 @@
-import { GET_LOGS , SET_LOADING , LOGS_ERROR , ADD_LOGS , DELETE_LOGS } from './types';
+import { 
+  GET_LOGS , 
+  SET_LOADING , 
+  LOGS_ERROR , 
+  ADD_LOGS , 
+  DELETE_LOGS, 
+  SET_CURRENT , 
+  CLEAR_CURRENT,
+  UPDATE_LOGS } from './types';
 
 //GET
 export const getLogs = () => async dispatch => {
@@ -63,6 +71,46 @@ export const deleteLogs = (id) => async dispatch => {
     dispatch({
       type: DELETE_LOGS ,
       payload: id
+    });
+  } 
+  catch (error) {
+    dispatch({
+      type: LOGS_ERROR ,
+      payload: error.data.response
+    });
+  }
+}
+
+//CURRENT Item
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT ,
+    payload: log
+  }
+}
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT 
+  }
+}
+
+//UPDATE
+export const updateLogs = (log) => async dispatch => {
+  try {
+    setLoading();
+    const req = await fetch(`/logs/${log.id}` , { 
+      method: 'PUT' ,
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(log)
+    });
+    
+    const data = await req.json();
+    
+    dispatch({
+      type: UPDATE_LOGS ,
+      payload: data
     });
   } 
   catch (error) {
